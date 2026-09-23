96c33570f54d2c272eb4690a560c4b25758d79b5
Rupak Kumar Das (Assistant Manager) <rupak@pksf.local>
2026-09-23 17:57:32 +0600

    recursion problem: digit count

--- src/RecursionOne.java
+++ src/RecursionOne.java
@@ -1,8 +1,15 @@
 public class RecursionOne {
 
     static void main() {
-        int n = 3;
-        IO.println(factorial(n));
+        IO.println(factorial(3));
+        IO.println(countDigit(123));
+    }
+
+    private static int countDigit(int n) {
+        if(n < 10) {
+            return 1;
+        }
+        return 1 + countDigit(n/10);
     }
 
     private static int factorial(int n) {
@@ -11,4 +18,5 @@ public class RecursionOne {
         }
         return n*factorial(n-1);
     }
+
 }
